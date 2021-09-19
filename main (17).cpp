9a5c0d55ee1fb174ec3bf8ea372bb46de3ac4545
ragini kumari <88733937+Raginikumari15@users.noreply.github.com>
2021-09-19 21:05:17 +0530

    Add files via upload

---
+++ main (17).cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;       /*1111
+                             2222     pattern;
+                             3333*/
+int main(){
+
+int j,i=1,n;
+int k=1;
+cin>>n;
+    while(i<=n){
+        j=1;
+        while(j<=n){
+           //int k= 1;
+            cout<< k;
+            j++;
+        }
+        cout<<endl;
+        k++;
+        i++;
+    }
+}
+    
